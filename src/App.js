e4e5861e9ee667e507a0413bd2c497149ecd8f3d
shubham <pshubham1886@gmail.com>
2022-07-17 00:21:24 +0530

    react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Details from './pages/Details';
+import AddEditBlog from './pages/AddEditBlog';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/detail/:id' element={<Details />} />
+        <Route path='/create' element={<AddEditBlog />} />
+        <Route path='/update/:id' element={<AddEditBlog />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div> 
   );
 }
 
