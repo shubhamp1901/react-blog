c9059d0bb9dbfcc40f3e8700402b90b368960961
shubham <pshubham1886@gmail.com>
2022-07-19 23:42:37 +0530

    create blog changes

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/detail/:id' element={<Details />} />
-        <Route path='/create' element={<AddEditBlog />} />
+        <Route path='/create' element={<AddEditBlog user={user} />} />
         <Route path='/update/:id' element={<AddEditBlog />} />
         <Route path='/about' element={<About />} />
         <Route path='/auth' element={<Auth setActive={setActive} />} />
