397b349a92a77748e237a11f9b4e95ef3d6b3a47
shubham <pshubham1886@gmail.com>
2022-07-20 23:22:29 +0530

    display blogs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,48 @@
-import React from 'react'
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import BlogSection from "../components/BlogSection";
+import { db } from "../firebase";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      collection(db, "blogs"),
+      (snapshot) => {
+        let list = [];
+        snapshot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setBlogs(list);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    return () => {
+      unsub()
+    }
+  }, []);
+
   return (
-    <div className='container-fluid pb-4 pt-4 padding'>
-      <div className='container padding'>
-          <div className='row mx-0'>
-            <h2>Trending</h2>
-            <div className='col-md-8'>
-              <h2>Blogs</h2>
-            </div>
-            <div className='col-md-3'>
-              <h2>Tags</h2>
-              <h2>Most Popular</h2>
-            </div>
+    <div className="container-fluid pb-4 pt-4 padding">
+      <div className="container padding">
+        <div className="row mx-0">
+          <h2>Trending</h2>
+          <div className="col-md-8">
+            <BlogSection blogs={blogs} />
           </div>
+          <div className="col-md-3">
+            <h2>Tags</h2>
+            <h2>Most Popular</h2>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
