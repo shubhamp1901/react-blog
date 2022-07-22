24e12493b607ba76e6120db0bd15237ebfd0c071
shubham <pshubham1886@gmail.com>
2022-07-23 01:08:01 +0530

    mobile responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import transitions from "bootstrap"; 
 
 const Header = ({ setActive, active, user, handleLogout }) => {
   const userId = user?.uid;
@@ -25,10 +26,9 @@ const Header = ({ setActive, active, user, handleLogout }) => {
             >
               <span className="fa fa-bars"></span>
             </button>
-            <div className="collapse navbar-collapse">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul
                 className="navbar-nav me-auto mb-2 mb-lg-0"
-                id="navbarSupportedContent"
               >
                 <Link to="/" style={{ textDecoration: "none" }}>
                   <li
