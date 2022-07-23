3bfb3947d99c9fe4349b7164b51dc803c673f0cf
shubham <pshubham1886@gmail.com>
2022-07-23 23:14:58 +0530

    firebase changes

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
+  apiKey: "AIzaSyB5svX-XmGOcLOhmzH8Kfx7JPCFEiS4N-8",
+  authDomain: "react-blog-app-a5bc9.firebaseapp.com",
+  projectId: "react-blog-app-a5bc9",
+  storageBucket: "react-blog-app-a5bc9.appspot.com",
+  messagingSenderId: "62354052112",
+  appId: "1:62354052112:web:84d08b7ff21600c7afa695",
 };
 
 // Initialize Firebase
